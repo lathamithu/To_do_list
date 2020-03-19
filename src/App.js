115ea16c9adac2b8a020f02385947f76b35085d5
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-03-19 22:51:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import Back from './components/back';
         <>
             <Navbar/>
                 <div id="sec"> 
-              
                     <div id="sec2">
-                        
                         <div id="sec3">
                         <Back/>
                             <div id="sec4">
-                            
                             <ul id="lst">
                             </ul>
                             </div>
